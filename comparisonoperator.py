d87d4400a2818edd5f1ce3b1d0f8b516b99ad565
shubhamhotchandani07-sudo <shubhamhotchandani07@gmail.com>
2026-06-22 04:55:30 -0700

    Testing

---
+++ comparisonoperator.py
@@ -0,0 +1,25 @@
+#a=10
+#b=50
+#print(a>b)
+
+#age=int(input("Enter a number"))
+#print(age>18)
+#print("\n")
+
+#marks=int(input("Enter a number"))
+#print(marks>40)
+
+#number=int(input("Enter a number"))
+#print(number%2==0)
+
+#number1=int(input("Enter a number"))
+#number2=int(input("Enter a number"))
+#print(number1==number2)
+
+#number1=int(input("Enter a number"))
+#number2=int(input("Enter a number"))
+#print(number1>number2)
+
+
+
+
