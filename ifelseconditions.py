3cf8288f004e04b23e37aeb4a2f2b879304d4011
shubhamhotchandani07-sudo <shubhamhotchandani07@gmail.com>
2026-06-22 23:41:30 -0700

    If else conditions programs

---
+++ ifelseconditions.py
@@ -0,0 +1,115 @@
+#AGE IF ELSE PROGRAM
+
+#age=int(input("Enter a age"))
+#if(age>=18):
+# print("eligible")
+#else:
+# print("not eligible")
+
+#MOBILE PASSWORD PROGRAM
+
+#password=int(input("Enter a number"))
+#if(password==0000):
+#  print("Correct password")
+#else:
+#  print("Incorrect password")
+
+#ASK FOR MARKS AND PRINT TRUE IF MARKS ARE 40 OR ABOVE
+
+#marks=int(input("Enter a marks"))
+#if(marks>40):
+# print("Pass")
+#else:
+# print("Fail")
+
+#EVEN NUMBER PROGRAM
+
+#number=int(input("Enter a number"))
+#if(number%2==0):
+# print("Even number")
+#else:
+ #print("Odd number")
+
+#NEGATIVE NUMBER CHECK PROGRAM
+
+#number=int(input("Enter a number"))
+#if(number<0):
+ #print("Negative number")
+#else:
+ #print("no negative number")
+
+#ASK FOR TWO NUMBER AND PRINT TRUE IF THEY ARE EQUAL
+
+#number1=int(input("Enter a number"))
+#number2=int(input("Enter a number"))
+#if(number1==number2):
+ #print("equal number")
+#else:
+ #print("not equal number")
+
+#PRINT TRUE IF FIRST NUMBER IS GREATER THAN SECOND
+
+#number1=int(input("Enter a number"))
+#number2=int(input("Enter a number"))
+#if(number1>number2):
+ #print("true")
+#else:
+ #print("False")
+
+#PRINT TURE IF NUMBER IS DIVISIBLE BY 5
+
+#number=int(input("Enter a number"))
+#if(number%5==0):
+ #print("true")
+#else:
+ #print("False")
+
+#ASK A NUMBER AND PRINT ITS SQUARE
+
+#number=int(input("Enter a number"))
+#if(number*number):
+ #print("true")
+#else:
+ #print("not")
+
+#PRINT TRUE IF SQUARE OF NUMBER IS GREATER THAN 100
+
+#number=int(input("Enter a number"))
+#if(number*number>100):
+ #print(True)
+#else:
+ #print("False")
+
+#PRINT TRUE IF NUMBER LEAVES REMAINER 1 BY DIVISIBLE FROM 3
+
+number=int(input("Enter a number"))
+if(number%3==1):
+ print("True")
+else:
+ print("False")
+
+#ASK MARKS IN 3 SUBJECTS AND PRINT TRUE IF AVERAGE ATLEAST 50
+
+marks1=int(input("Enter a marks"))
+marks2=int(input("Enter a marks"))
+marks3=int(input("Enter a marks"))
+average=(marks1+marks2+marks3)/3
+if(average>50):
+ print("True")
+else:
+ print("False")
+
+year=int(input("Enter a year"))
+if(year%4==0):
+ print("Leap year")
+else:
+ print("Not a leap year")
+
+
+
+
+
+
+
+
+
