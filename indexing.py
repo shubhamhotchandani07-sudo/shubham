25b5a47f53aac71d1c651cb6f012ec7813b7c6ea
shubhamhotchandani07-sudo <shubhamhotchandani07@gmail.com>
2026-06-29 23:47:33 -0700

    indexing Python

---
+++ indexing.py
@@ -0,0 +1,50 @@
+a="Shubham"
+print(a[3])
+
+b=["Virat","Rohit","Surya","Rahul"]
+print(b[2])
+
+c="Virat"
+print(c[-2])
+
+d=["Dhoni","Rohit","Hello","Delhi"]
+print(d[-1])
+
+t=(10,20,30,40,50)
+print(t[3])
+
+#Print the sum of the first and last elements
+nums=[20,30,40,50,70]
+print(nums[2]+nums[-3])
+
+#Print the first and last letters of
+word="Programming"
+print(word[0],word[-1])
+
+colors=["Red","Blue","Green","Purple"]
+print(colors[0],colors[-1])
+
+#Print the highest mark using indexing only
+marks=[85,90,78,92,88]
+print(marks[3])
+
+#print crmp from computer word
+s="computer"
+print(s[0],s[-1],s[2],s[3])
+
+w="samsung"
+print(w[0]) 
+print(w[1])
+print(w[2])
+print(w[3])
+print(w[4])
+print(w[5])
+print(w[6])
+
+z=["Rahul",19,"BCA",85]
+print("Name:",z[0])
+print("Course:",z[2])
+print("Marks:",z[-1])
+
+o="Shubham"
+print(0[10])
